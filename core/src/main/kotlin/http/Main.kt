7c0a5f7bdcdee17a31a458f9626647df458a4c52
RichoDemus <richodemus@gmail.com>
2018-09-03 22:53:19 +0200

    Set max connections per destination to 100 000

--- core/src/main/kotlin/http/Main.kt
+++ core/src/main/kotlin/http/Main.kt
@@ -3,12 +3,15 @@ package http
 import com.google.common.base.Stopwatch
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
 import kotlinx.coroutines.experimental.withContext
 import org.eclipse.jetty.client.HttpClient
 import org.eclipse.jetty.client.api.Result
 import org.eclipse.jetty.client.util.BufferingResponseListener
 import org.eclipse.jetty.util.ssl.SslContextFactory
+import java.util.concurrent.atomic.LongAdder
 import kotlin.coroutines.experimental.suspendCoroutine
 
 
@@ -18,17 +21,39 @@ fun main(args: Array<String>) {
     httpClient = HttpClient(SslContextFactory())
     try {
         httpClient.isFollowRedirects = false
+        httpClient.maxConnectionsPerDestination = 100_000
+        httpClient.maxRequestsQueuedPerDestination = 100_000
         httpClient.start()
 
+        val started = LongAdder()
+        val done = LongAdder()
+        var running = true
         runBlocking {
-            val watch = Stopwatch.createStarted()
-            val results = List(1000) {
-                async { http("https://richodemus.com")}
+            launch {
+                while (running) {
+                    println("${started.toLong()} started and ${done.toLong()} done")
+                    delay(1000L)
+                }
             }
 
-            val asdf = results.map { it.await() }.map { String(it) }
+            val watch = Stopwatch.createStarted()
+            val results = List(10_000) {
+                async {
+                    val result = http("http://localhost:8080")
+                    started.increment()
+                    result
+                }
+            }
+
+            val asdf = results.map {
+                val res = it.await()
+                done.increment()
+                res
+            }.map { String(it) }
             watch.stop()
             println("Got ${asdf.size} responses in $watch")
+            running = false
+            println("${started.toLong()} started and ${done.toLong()} done")
         }
 
 
@@ -47,6 +72,8 @@ private suspend fun http(url: String) =
                                 if (!result!!.isFailed) {
                                     val responseContent = content
                                     continuation.resume(responseContent)
+                                } else {
+                                    continuation.resumeWithException(RuntimeException(result.failure))
                                 }
                             }
                         })
